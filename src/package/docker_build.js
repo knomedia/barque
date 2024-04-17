c3797de1853df121a068fdfa829412f607a56495
jsk <knomedia@gmail.com>
2024-04-16 19:03:58 -0600

    create default platform for docker build
    default to linux/amd64
    
    Can choose a different platform via upmk.yaml config platform

--- src/package/docker_build.js
+++ src/package/docker_build.js
@@ -13,17 +13,32 @@ function buildArgs(npmToken, envMode) {
     return `--build-arg NPM_TOKEN=${npmToken} --build-arg ENV_MODE=${envMode}`
   }
 }
-function buildCMD(image, npmToken, envMode) {
+
+function buildPlatform(env) {
+  let platform = "linux/amd64"
+  try {
+    if (env.platform) {
+      platform = env.platform
+    }
+  } catch(e) {
+    console.log('error accessing platform key from env')
+    console.log(env);
+  }
+  return `--platform=${platform}`
+}
+
+function buildCMD(image, env, npmToken, envMode) {
   let args = buildArgs(npmToken, envMode)
-  let cmd = `docker build -t ${image}`
+  let platform = buildPlatform(env)
+  let cmd = `docker build ${platform} -t ${image}`
   if (args) {
     cmd += " " + args
   }
   cmd += " ."
   return cmd
 }
-async function build(tb, image, npmToken, envMode) {
-  let cmd = buildCMD(image, npmToken, envMode)
+async function build(tb, image, settings, npmToken, envMode) {
+  let cmd = buildCMD(image, settings, npmToken, envMode)
   tb.print.info(cmd)
   let output = await tb.system.run(cmd)
   tb.print.info(output)
@@ -36,9 +51,9 @@ async function push(tb, image) {
   tb.print.info(output)
 }
 
-async function dockerPackage(tb, version, settings, npmToken, envMode) {
+async function dockerPackage(tb, version, settings, env, npmToken, envMode) {
   let image = buildImageName(settings, version)
-  await build(tb, image, npmToken, envMode)
+  await build(tb, image, env, npmToken, envMode)
   return image
 }
 
